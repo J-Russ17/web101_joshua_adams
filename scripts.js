d6ad6e58203b1b8e2eeb2c3dd97ea3393c2a3fd0
J-Russ17 <j.adams5435@gmail.com>
2023-07-25 12:35:46 -0500

    contactPage

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+let showInConsole
+
+function inbox(input) {
+showInConsole = input;
+console.log({showInConsole});
+}
+
+function sure() {
+  confirm("This will get rid of your progress do you still wish to continue?")
+}
+
+function check() {
+  document.getElementById("classmate")
+  console.log("Nice you checked a box!")
+}
